d4eda7c9f0fcb033d423f354de1851a66d07fa4b
khazhyk <khazhy@gmail.com>
2017-07-04 22:26:56 -0700

    enable auto-sharding

--- dango/main.py
+++ dango/main.py
@@ -23,6 +23,6 @@ def setup_logging():
 
 def main(config):
     setup_logging()
-    bot = core.DangoAutoShardedBot('test ', shard_count=1, config=config)
+    bot = core.DangoAutoShardedBot('test ', config=config)
     bot.watch_plugin_dir(getattr(config, 'plugins', 'plugins'))  # TODO
     bot.run(config.token)
