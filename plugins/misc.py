29c9e7f3941ff2185c62ba310133a7fca3f63015
khazhyk <khazhy@gmail.com>
2017-09-09 22:22:44 -0700

    Add msgsource msg

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,5 +1,8 @@
 from dango import dcog
+from dango import utils
+import discord
 from discord.ext.commands import command
+from discord.ext.commands import errors
 
 FULLWIDTH_OFFSET = 65248
 
@@ -16,3 +19,12 @@ class Misc:
         await ctx.send("".join(map(
             lambda c: chr(ord(c) + FULLWIDTH_OFFSET) if (ord(c) >= 0x21 and ord(c) <= 0x7E) else c,
             msg)).replace(" ", chr(0x3000)))
+
+    @command()
+    async def msgsource(self, ctx, *, msg_id: int):
+        try:
+            msg = await ctx.get_message(msg_id)
+        except discord.NotFound:
+            raise errors.BadArgument("Message not found")
+        else:
+            await ctx.send("```{}```".format(utils.clean_triple_backtick(msg.content)))
