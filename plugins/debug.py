c7c9b8ec80778dff96828bc82ba876a07903fe33
khazhyk <khazhy@gmail.com>
2017-11-19 21:32:46 -0800

    Add as cmd, fix some permissions.

--- plugins/debug.py
+++ plugins/debug.py
@@ -1,4 +1,5 @@
 import asyncio
+import copy
 import logging
 import os
 import sys
@@ -7,6 +8,7 @@ import aiohttp
 from dango import checks
 from dango import dcog
 from dango import utils
+import discord
 from discord.ext.commands import command
 
 log = logging.getLogger(__name__)
@@ -41,11 +43,13 @@ class Debug:
         await ctx.send("\N{AUBERGINE}")
 
     @command()
+    @checks.is_owner()
     async def die(self, ctx):
         await ctx.message.add_reaction(":discordok:293495010719170560")
         await ctx.bot.logout()
 
     @command()
+    @checks.is_owner()
     async def restart(self, ctx):
         await ctx.message.add_reaction(":helYea:236243426662678528")
         print(['python'] + sys.argv, sys.executable)
@@ -63,6 +67,19 @@ class Debug:
             out = stdout
 
         await ctx.send("```{}```".format(out))
+
+    @command(name="as")
+    @checks.is_owner()
+    async def as_(self, ctx, who: discord.Member, *, cmd):
+        """Run a command impersonating another user."""
+        fake_msg = copy.copy(ctx.message)
+
+        # msg._update handles clearing cached properties
+        fake_msg._update(ctx.message.channel, dict(
+            content=ctx.prefix + cmd))
+        fake_msg.author = who
+        new_ctx = await ctx.bot.get_context(fake_msg)
+        await ctx.bot.invoke(new_ctx)
 
     @command()
     @checks.is_owner()
