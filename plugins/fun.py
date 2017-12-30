f358d49f2d021747996af57ff1f79281a4fb860d
khazhyk <khazhy@gmail.com>
2017-12-29 23:01:07 -0500

    Add dot.
    Also move corrupt to imgfun.

--- plugins/fun.py
+++ plugins/fun.py
@@ -12,12 +12,14 @@ Tag types:
  - Custom tags
    - Python custom image generation (everything else).
 """
+import io
 import os
 import random
 
 from dango import dcog
 import discord
-from discord.ext.commands import group
+from discord.ext.commands import command, group
+from PIL import Image
 
 ALLOWED_EXT = {'jpg', 'jpeg', 'png', 'gif', 'webp'}
 
@@ -87,3 +89,37 @@ class Fun:
                 cmd = ImgFileCmd(os.path.splitext(item)[0], fullpath)
             self.meme.add_command(cmd)
             bot.add_command(cmd)
+
+@dcog()
+class ImgFun:
+
+    def __init__(self, cfg):
+        pass
+
+    @command()
+    async def corrupt(self, ctx, *, user: discord.User=None):
+        """Corrupt a user's avatar."""
+        user = user or ctx.message.author
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(user.avatar_url_as(format='jpg')) as resp:
+                img_buff = bytearray(await resp.read())
+        for i in range(random.randint(5, 25)):
+            img_buff[random.randint(0, len(img_buff))] = random.randint(1, 254)
+        await ctx.send(file=discord.File(io.BytesIO(img_buff), filename="img.jpg"))
+
+
+    @staticmethod
+    def make_dot():
+        width = 512
+        img = Image.new('RGBA', (width, 1))
+        img.putpixel((random.randrange(0, width), 0), 0xff0000ff)
+        buff = io.BytesIO()
+        img.save(buff, 'png')
+        buff.seek(0)
+        return buff
+
+
+    @command()
+    async def dot(self, ctx):
+        res = await ctx.bot.loop.run_in_executor(None, self.make_dot)
+        await ctx.send(file=discord.File(res, filename="dot.png"))
--- plugins/misc.py
+++ plugins/misc.py
@@ -336,14 +336,3 @@ class Misc:
 
         await ctx.send("```json\n{}```".format(
             utils.clean_triple_backtick(json.dumps(raw, indent=2))))
-
-    @command()
-    async def corrupt(self, ctx, *, user: discord.User=None):
-        """Corrupt a user's avatar."""
-        user = user or ctx.message.author
-        async with aiohttp.ClientSession() as sess:
-            async with sess.get(user.avatar_url_as(format='jpg')) as resp:
-                img_buff = bytearray(await resp.read())
-        for i in range(random.randint(5, 25)):
-            img_buff[random.randint(0, len(img_buff))] = random.randint(1, 254)
-        await ctx.send(file=discord.File(io.BytesIO(img_buff), filename="img.jpg"))
