e613d4457045c131dc212686e74c325418fd1399
khazhyk <khazhy@gmail.com>
2018-01-06 01:33:42 -0500

    Use normal avatar url

--- plugins/info.py
+++ plugins/info.py
@@ -266,11 +266,7 @@ class Info:
         if user is None:
             user = ctx.message.author
 
-        avatar_url = user.avatar_url_as(static_format='png')
-        if user.is_avatar_animated() and not avatar_url.endswith('.gif'):
-            avatar_url += "&discord_is_buggy_af=.gif"
-
-        await ctx.send(avatar_url)
+        await ctx.send(user.avatar_url_as(static_format='png'))
 
     @command()
     async def defaultavatar(self, ctx, *, user: discord.User=None):
