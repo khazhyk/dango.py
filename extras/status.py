1438823860214ee9bcacc4b2d9b8dc92ce13920e
khazhyk <khazhy@gmail.com>
2018-07-14 13:47:10 -0700

    wait_until_ready seems busted

--- extras/status.py
+++ extras/status.py
@@ -29,7 +29,9 @@ class Status:
 
         create_task(self.update_presence())
 
+    async def on_ready(self):
+        await self.update_presence()
+
     async def update_presence(self):
-        await self.bot.wait_until_ready()
         await self.bot.change_presence(
             status=discord.Status.invisible, afk=True)
